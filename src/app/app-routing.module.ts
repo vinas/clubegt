e09fbd48c413ad0583a84b0de25075e7be5141c6
Vinas <vandrade@vstsalelo.onmicrosoft.com>
2019-02-26 08:05:24 -0300

    added messages service and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DriversComponent } from './drivers/drivers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'drivers', component: DriversComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/driver.service.ts
+++ src/app/driver.service.ts
@@ -2,15 +2,17 @@ import { Injectable } from '@angular/core';
 import { Driver } from './driver';
 import { DRIVERS } from './mock-drivers';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DriverService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getDriversList(): Observable<Driver[]> {
+    this.messageService.add('teste bunda');
     return of(DRIVERS);
   }
 
